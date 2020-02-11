2a24f50fc4d32ae0c6a644729b9118493d1fa4d6
sparkyboy161 <46911331+sparkyboy161@users.noreply.github.com>
2020-02-11 15:03:52 +0700

    add antd and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Layout } from 'antd';
+
+const { Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        <Sider style={{ backgroundColor: 'blue' }} width='100'>
+          <p>h1</p>
+        </Sider>
+        <Content style={{ backgroundColor: 'red' }}>
+          <p>h2</p>
+        </Content>
+      </Layout>
     </div>
   );
 }
